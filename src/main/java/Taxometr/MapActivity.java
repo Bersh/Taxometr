50e33f09b9665325c3b5d29f94748f578ecb60c1
Iliya Bershadsky <ibersh20@gmail.com>
2012-03-15 17:55:59 +0200

    Empty project

---
+++ src/main/java/Taxometr/MapActivity.java
@@ -0,0 +1,15 @@
+package Taxometr;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author ibershadskiy <a href="mailto:Ilya.Bershadskiy@exigenservices.com">Ilya Bershadskiy</a>
+ * @since 15.03.12
+ */
+public class MapActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.main);
+    }
+}
