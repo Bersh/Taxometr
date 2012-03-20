0e1a821c311b5d6a554dc34263c97c03856e0887
Iliya Bershadsky <ibersh20@gmail.com>
2012-03-20 16:43:56 +0200

    current position indication

--- src/main/java/com/taxometr/activites/MainActivity.java
+++ src/main/java/com/taxometr/activites/MainActivity.java
@@ -1,82 +1,57 @@
 package com.taxometr.activites;
 
+import java.util.List;
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+import com.google.android.maps.MyLocationOverlay;
+import com.taxometr.R;
+import com.taxometr.helpers.LocationHelper;
 import android.content.Context;
-import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.location.LocationProvider;
 import android.os.Bundle;
 import android.widget.Toast;
-import com.google.android.maps.GeoPoint;
-import com.google.android.maps.MapActivity;
-import com.google.android.maps.MapController;
-import com.google.android.maps.MapView;
-import com.taxometr.R;
-import com.taxometr.helpers.LocationHelper;
-
-import java.util.List;
 
 /**
  * @author ibershadskiy <a href="mailto:iBersh20@gmail.com">Ilya Bershadskiy</a>
  * @since 15.03.12
  */
 public class MainActivity extends MapActivity {
+    private static final int MIN_UPDATE_TIME = 3000;
     private MapController mapController;
     private LocationManager locationManager;
 
-    private LocationProvider locationProvider;
     private MapView mapView;
-    private Drawable defaultMarker;
     private String locationProviderType;
 
-    private final LocationListener locationListenerRecenterMap =
-            new LocationListener() {
-                public void onLocationChanged(final Location loc) {
-                    int lat = (int) (loc.getLatitude()
-                            * LocationHelper.MILLION);
-                    int lon = (int) (loc.getLongitude()
-                            * LocationHelper.MILLION);
-                    GeoPoint geoPoint = new GeoPoint(lat, lon);
-                    mapController.animateTo(geoPoint);
-                }
-
-                public void onProviderDisabled(String s) {
-                }
-
-                public void onProviderEnabled(String s) {
-                }
-
-                public void onStatusChanged(String s,
-                                            int i, Bundle b) {
-                }
-            };
+    private final LocationListener locationListenerRecenterMap = new LocationTrackingListener();
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-        this.setContentView(R.layout.main);
-        this.mapView = (MapView) this.findViewById(R.id.map_view);
-
-        this.defaultMarker = getResources().getDrawable(R.drawable.red_pin);
-        this.defaultMarker.setBounds(0, 0,
-                this.defaultMarker.getIntrinsicWidth(),
-                this.defaultMarker.getIntrinsicHeight());
+        setContentView(R.layout.main);
+        mapView = (MapView) this.findViewById(R.id.map_view);
+        mapView.setBuiltInZoomControls(true);
     }
 
     @Override
     protected boolean isRouteDisplayed() {
-        return false;  //TODO Implement
+        return false;
     }
 
     @Override
     public void onStart() {
         super.onStart();
+
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         if (locationManager == null) {
             this.finish();
         }
-        List<String> enabledProviders = locationManager.getProviders(true);
+        final List<String> enabledProviders = locationManager.getProviders(true);
         if (enabledProviders.contains(LocationManager.GPS_PROVIDER)) {
             locationProviderType = LocationManager.GPS_PROVIDER;
         } else if (enabledProviders.contains(LocationManager.NETWORK_PROVIDER)) {
@@ -84,10 +59,10 @@ public class MainActivity extends MapActivity {
         } else {
             locationProviderType = LocationManager.PASSIVE_PROVIDER;
         }
-        locationProvider = this.locationManager.getProvider(locationProviderType);
+
+        final LocationProvider locationProvider = this.locationManager.getProvider(locationProviderType);
         if (locationProvider != null) {
-            this.locationManager.requestLocationUpdates(
-                    locationProvider.getName(), 3000, 1000,
+            this.locationManager.requestLocationUpdates(locationProvider.getName(), MIN_UPDATE_TIME, 1000,
                     this.locationListenerRecenterMap);
         } else {
             Toast.makeText(this, "Taxometr cannot continue,"
@@ -95,22 +70,57 @@ public class MainActivity extends MapActivity {
                     + " at this time.", Toast.LENGTH_SHORT).show();
             this.finish();
         }
-        GeoPoint lastKnownPoint = getLastKnownPoint();
-        this.mapController = this.mapView.getController();
-        this.mapController.setZoom(10);
-        this.mapController.animateTo(lastKnownPoint);
+
+        final MyLocationOverlay myLocationOverlay = new MyLocationOverlay(this, mapView);
+        myLocationOverlay.disableCompass();
+        myLocationOverlay.enableMyLocation();
+        mapView.getOverlays().add(myLocationOverlay);
+
+        final GeoPoint lastKnownPoint = getLastKnownPoint();
+        mapController = this.mapView.getController();
+        mapController.setZoom(10);
+        mapController.animateTo(lastKnownPoint);
     }
 
+    /**
+     * Converts last known point from LocationManager to GeoPoint
+     *
+     * @return GeoPoint coresponds to last known point from LocationManager
+     */
     private GeoPoint getLastKnownPoint() {
-        GeoPoint lastKnownPoint;
-        Location lastKnownLocation = this.locationManager.getLastKnownLocation(locationProviderType);
+        final GeoPoint lastKnownPoint;
+        final Location lastKnownLocation = this.locationManager.getLastKnownLocation(locationProviderType);
         if (lastKnownLocation != null) {
             lastKnownPoint = LocationHelper.getGeoPoint(lastKnownLocation);
         } else {
-            lastKnownPoint = LocationHelper.GOLDEN_GATE;
+            lastKnownPoint = LocationHelper.DEFAULT_LOCATION;
         }
         return lastKnownPoint;
     }
 
+    /**
+     * LocationListener to track location changes
+     */
+    private class LocationTrackingListener implements LocationListener {
+        @Override
+        public void onLocationChanged(final Location loc) {
+            final int lat = (int) (loc.getLatitude() * LocationHelper.MILLION);
+            final int lon = (int) (loc.getLongitude() * LocationHelper.MILLION);
+            final GeoPoint geoPoint = new GeoPoint(lat, lon);
+            mapController.animateTo(geoPoint);
+            mapController.setCenter(geoPoint);
+        }
 
+        @Override
+        public void onProviderDisabled(String s) {
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle b) {
+        }
+    }
 }
--- src/main/java/com/taxometr/helpers/LocationHelper.java
+++ src/main/java/com/taxometr/helpers/LocationHelper.java
@@ -1,52 +1,84 @@
 package com.taxometr.helpers;
 
+import java.text.DecimalFormat;
+import com.google.android.maps.GeoPoint;
 import android.location.Location;
 import android.util.Log;
 
-import com.google.android.maps.GeoPoint;
-
-import java.text.DecimalFormat;
-
+/**
+ * useful functions and constants for location
+ * @author ibershadskiy <a href="mailto:iBersh20@gmail.com">Ilya Bershadskiy</a>
+ */
 public class LocationHelper {
-
+    /**
+     * Class tag fro logging
+     */
     public static final String CLASSTAG = LocationHelper.class.getSimpleName();
 
+    /**
+     * million
+     */
     public static final double MILLION = 1e6;
 
     private static final DecimalFormat DEC_FORMAT = new DecimalFormat("###.##");
 
-    public static final GeoPoint GOLDEN_GATE = new GeoPoint((int) (37.49 * LocationHelper.MILLION),
-            (int) (-122.49 * LocationHelper.MILLION));
-    
+    /**
+     * Golden Gate location
+     */
+    public static final GeoPoint DEFAULT_LOCATION = new GeoPoint((int) (30.30 * LocationHelper.MILLION),
+            (int) (50.27 * LocationHelper.MILLION));
+
+    /**
+     * log tag fro logging
+     */
     private static final String LOGTAG = "taxometr";
 
-    // note GeoPoint stores lat/long as "integer numbers of microdegrees"
-    // meaning int*1E6
-    // parse Location into GeoPoint
+    /**
+     * Default constructor
+     */
+    private LocationHelper() {
+    }
+
+    /**
+     * Parse Location into GeoPoint  <br/>
+     * note GeoPoint stores lat/long as "integer numbers of microdegrees"
+     * meaning int*1E6
+     * @param loc instance of {@link android.location.Location}
+     * @return {@link com.google.android.maps.GeoPoint} converted from given location
+     */
     public static GeoPoint getGeoPoint(final Location loc) {
-        int lat = (int) (loc.getLatitude() * LocationHelper.MILLION);
-        int lon = (int) (loc.getLongitude() * LocationHelper.MILLION);
+        final int lat = (int) (loc.getLatitude() * LocationHelper.MILLION);
+        final int lon = (int) (loc.getLongitude() * LocationHelper.MILLION);
         return new GeoPoint(lat, lon);
     }
 
-    // parse geoRssPoint into GeoPoint(<georss:point>36.835 -121.899</georss:point>)
+    /**
+     * Parse geoRssPoint into GeoPoint(<georss:point>36.835 -121.899</georss:point>)
+     * @param geoRssPoint geo point in rss format
+     * @return {@link com.google.android.maps.GeoPoint} converted from rss format
+     */
     public static GeoPoint getGeoPoint(final String geoRssPoint) {
         Log.d(LOGTAG, LocationHelper.CLASSTAG + " getGeoPoint - geoRssPoint - " + geoRssPoint);
         GeoPoint returnPoint = null;
-        String gPoint = geoRssPoint.trim();
+        final String gPoint = geoRssPoint.trim();
         if (gPoint.contains(" ")) {
-            String latString = gPoint.substring(0, gPoint.indexOf(" "));
-            String lonString = gPoint.substring(gPoint.indexOf(" "), gPoint.length());
-            double latd = Double.parseDouble(latString);
-            double lond = Double.parseDouble(lonString);
-            int lat = (int) (latd * LocationHelper.MILLION);
-            int lon = (int) (lond * LocationHelper.MILLION);
+            final String latString = gPoint.substring(0, gPoint.indexOf(" "));
+            final String lonString = gPoint.substring(gPoint.indexOf(" "), gPoint.length());
+            final double latd = Double.parseDouble(latString);
+            final double lond = Double.parseDouble(lonString);
+            final int lat = (int) (latd * LocationHelper.MILLION);
+            final int lon = (int) (lond * LocationHelper.MILLION);
             returnPoint = new GeoPoint(lat, lon);
         }
         return returnPoint;
     }
 
-    // parse double point(-127.50) into String (127.50W)
+    /**
+     * Parse double point(-127.50) into String (127.50W)
+     * @param point double cordinate
+     * @param isLat is latitude
+     * @return string coordinate
+     */
     public static String parsePoint(final double point, final boolean isLat) {
         Log.d(LOGTAG, LocationHelper.CLASSTAG + " parsePoint - point - " + point + " isLat - " + isLat);
         String result = LocationHelper.DEC_FORMAT.format(point);
