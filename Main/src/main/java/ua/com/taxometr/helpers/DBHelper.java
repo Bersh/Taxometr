07bb96ad7fdf7138e61f8e78b25f980dcb8751bf
Iliya Bershadsky <ibersh20@gmail.com>
2012-04-18 11:23:37 +0300

    DBHelper refactoring

--- Main/src/main/java/ua/com/taxometr/helpers/DBHelper.java
+++ Main/src/main/java/ua/com/taxometr/helpers/DBHelper.java
@@ -37,54 +37,70 @@ public class DBHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         try {
             //create database structure
-            db.execSQL("create table countries ("
-                    + "_id integer primary key autoincrement, "
-                    + "name_rus text, "
-                    + "name_en text);");
-
-            db.execSQL("create table cities ("
-                    + "_id integer primary key autoincrement, "
-                    + "name_rus text, "
-                    + "name_en text, "
-                    + "country_id integer, "
-                    + "foreign key(country_id) references countries(_id));");
-
-            db.execSQL("create table taxi_services ("
-                    + "_id integer primary key autoincrement, "
-                    + "name_rus text, "
-                    + "name_en text, "
-                    + "city_id integer, "
-                    + "foreign key(city_id) references cities(_id));");
-
-            db.execSQL("create table phones ("
-                    + "_id integer primary key autoincrement, "
-                    + "phone text, "
-                    + "taxi_id integer, "
-                    + "foreign key(taxi_id) references taxi_services(_id));");
-
-            //create sample data
-            db.execSQL("insert into countries (_id, name_rus, name_en) values(0, \"Россия\", \"Russia\")");
-            db.execSQL("insert into countries (_id, name_rus, name_en) values(1, \"Украина\", \"Ukraine\")");
-
-            db.execSQL("insert into cities (_id, name_rus, name_en, country_id) values(0, \"Москва\", \"Moscow\", 0)");
-            db.execSQL("insert into cities (_id, name_rus, name_en, country_id) values(1, \"Киев\", \"Kiev\", 1)");
-
-
-            db.execSQL("insert into taxi_services (_id, name_rus, name_en, city_id) values(0, \"Мегаполис\", \"Megapolis\", 1)");
-            db.execSQL("insert into taxi_services (_id, name_rus, name_en, city_id) values(1, \"Каприз\", \"Kapriz\", 1)");
-            db.execSQL("insert into taxi_services (_id, name_rus, name_en, city_id) values(3, \"Хит такси\", \"Hit taxi\", 0)");
-
-            db.execSQL("insert into phones (phone, taxi_id) values(\"+380939125432\", 0)");
-            db.execSQL("insert into phones (phone, taxi_id) values(\"+380931111111\", 0)");
-            db.execSQL("insert into phones (phone, taxi_id) values(\"+380504512365\", 0)");
-            db.execSQL("insert into phones (phone, taxi_id) values(\"+380674512395\", 1)");
-            db.execSQL("insert into phones (phone, taxi_id) values(\"+380502315276\", 2)");
+            createDBStructure(db);
+            createSampleData(db);
             Log.e("Taxometr_DB", "Tables created!");
         } catch (SQLException e) {
             Log.e("Taxometr_DB", e.getMessage(), e);
             Log.e("Taxometr_DB", "Tasdgklsdjklgjsdklgjklsdjgklsdjklhgsdjgvkla");
             throw e;
         }
+    }
+
+    /**
+     * Creates database structure
+     * @param db current database
+     * @throws android.database.SQLException if any query fails
+     */
+    private static void createDBStructure(SQLiteDatabase db) throws SQLException{
+        db.execSQL("create table countries ("
+                + "_id integer primary key autoincrement, "
+                + "name_rus text, "
+                + "name_en text);");
+
+        db.execSQL("create table cities ("
+                + "_id integer primary key autoincrement, "
+                + "name_rus text, "
+                + "name_en text, "
+                + "country_id integer, "
+                + "foreign key(country_id) references countries(_id));");
+
+        db.execSQL("create table taxi_services ("
+                + "_id integer primary key autoincrement, "
+                + "name_rus text, "
+                + "name_en text, "
+                + "city_id integer, "
+                + "foreign key(city_id) references cities(_id));");
+
+        db.execSQL("create table phones ("
+                + "_id integer primary key autoincrement, "
+                + "phone text, "
+                + "taxi_id integer, "
+                + "foreign key(taxi_id) references taxi_services(_id));");
+    }
+
+    /**
+     * Creates sample data
+     * @param db current database
+     * @throws android.database.SQLException if any query fails
+     */
+    private static void createSampleData(SQLiteDatabase db) throws SQLException{
+        db.execSQL("insert into countries (_id, name_rus, name_en) values(0, \"Россия\", \"Russia\")");
+        db.execSQL("insert into countries (_id, name_rus, name_en) values(1, \"Украина\", \"Ukraine\")");
+
+        db.execSQL("insert into cities (_id, name_rus, name_en, country_id) values(0, \"Москва\", \"Moscow\", 0)");
+        db.execSQL("insert into cities (_id, name_rus, name_en, country_id) values(1, \"Киев\", \"Kiev\", 1)");
+
+
+        db.execSQL("insert into taxi_services (_id, name_rus, name_en, city_id) values(0, \"Мегаполис\", \"Megapolis\", 1)");
+        db.execSQL("insert into taxi_services (_id, name_rus, name_en, city_id) values(1, \"Каприз\", \"Kapriz\", 1)");
+        db.execSQL("insert into taxi_services (_id, name_rus, name_en, city_id) values(3, \"Хит такси\", \"Hit taxi\", 0)");
+
+        db.execSQL("insert into phones (phone, taxi_id) values(\"+380939125432\", 0)");
+        db.execSQL("insert into phones (phone, taxi_id) values(\"+380931111111\", 0)");
+        db.execSQL("insert into phones (phone, taxi_id) values(\"+380504512365\", 0)");
+        db.execSQL("insert into phones (phone, taxi_id) values(\"+380674512395\", 1)");
+        db.execSQL("insert into phones (phone, taxi_id) values(\"+380502315276\", 2)");
     }
 
     @Override
