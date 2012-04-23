69a421046a158e5a9050dc4d6794cf366224aab8
Dmitriy Gorbunov <divgorbunov@gmail.com>
2012-04-23 21:24:31 +0300

    CallActivity has been changed

--- Main/src/main/java/ua/com/taxometr/activites/CallActivity.java
+++ Main/src/main/java/ua/com/taxometr/activites/CallActivity.java
@@ -13,10 +13,9 @@ import android.util.Log;
 import android.content.ActivityNotFoundException;
 
 /**
- *
- * User: Kardan
  * Date: 02.04.12
- * Time: 17:49
+ * @author divgorbunov <a href="mailto:divgorbunov@gmail.com">Dmitriy Gorbunov</a>
+ * @since 02.04.12
  */
 public class CallActivity extends Activity{
 
@@ -25,21 +24,32 @@ public class CallActivity extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.call_view);
         final String phoneNumber = getIntent().getStringExtra("phoneNumber");
-        TextView lblNumber = (TextView) findViewById(R.id.lbl_call_number);
+        final TextView lblNumber = (TextView) findViewById(R.id.lbl_call_number);
         lblNumber.setText(phoneNumber);
 
-        Button btnAccept = (Button) findViewById(R.id.btn_accept_call);
+        final Button btnAccept = (Button) findViewById(R.id.btn_accept_call);
         btnAccept.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                call("tel:" + phoneNumber);
+                //call();
+                try
+                {
+                    final Intent callIntent = new Intent(Intent.ACTION_CALL);
+                    callIntent.setData(Uri.parse("tel:" + phoneNumber));
+                    startActivity(callIntent);
+                }
+                catch (ActivityNotFoundException activityException)
+                {
+                    Log.e("Error", "Call failed", activityException);
+                }
+
                 final Intent intent = new Intent();
                 setResult(RESULT_OK, intent);
                 finish();
             }
         });
 
-        Button btnCancel = (Button) findViewById(R.id.btn_cancel_call);
+        final Button btnCancel = (Button) findViewById(R.id.btn_cancel_call);
 
         btnCancel.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,20 +59,6 @@ public class CallActivity extends Activity{
                 finish();
             }
         });
-
-    }
-    private void call(String phoneNumber)
-    {
-        try
-        {
-            Intent callIntent = new Intent(Intent.ACTION_CALL);
-            callIntent.setData(Uri.parse(phoneNumber));
-            startActivity(callIntent);
-        }
-        catch (ActivityNotFoundException activityException)
-        {
-            Log.e("Error", "Call failed", activityException);
-        }
 
     }
 
